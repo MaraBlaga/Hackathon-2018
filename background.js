86c1108f734ccc8782399822c71a0357da75ca35
AD\mark.smith <mark.smith@maruedr.com>
2018-05-18 09:34:43 +0100

    Fixed issue with "undefined" appearing under host

--- background.js
+++ background.js
@@ -9,21 +9,26 @@ function surveyAlreadyAdded(newSurvey, surveys) {
 }
 
 chrome.runtime.onMessage.addListener(
-    function(survey, sender, callback) {
-        chrome.storage.sync.get({
-            // Key: default value if not set
-            surveyList: [],
-        }, function(data) {
-            if (surveyAlreadyAdded(survey, data.surveyList)) {
-                callback({message: 'This survey has already been added'});
-                return;
-            }
+    function(request, sender, callback) {
+        switch (request.type) {
+            case "notification":
+                let survey = request.options;
+                chrome.storage.sync.get({
+                    // Key: default value if not set
+                    surveyList: [],
+                }, function(data) {
+                    if (surveyAlreadyAdded(survey, data.surveyList)) {
+                        callback({message: 'This survey has already been added'});
+                        return;
+                    }
 
-            data.surveyList.push(survey);
-            chrome.storage.sync.set({surveyList: data.surveyList}, function() {
-                callback({message: 'Survey added', count: data.surveyList.length});
-            });
-        });
+                    data.surveyList.push(survey);
+                    chrome.storage.sync.set({surveyList: data.surveyList}, function() {
+                        callback({message: 'Survey added', count: data.surveyList.length});
+                    });
+                });
+                break;
+        }
 
         return true;
     }
